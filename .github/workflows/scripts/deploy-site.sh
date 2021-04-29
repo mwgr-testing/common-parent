dbf0ac2632bbb9a822bfb7502320add9ea53fa6a
Mirko Wagner <seelenvirtuose@gmail.com>
2021-04-29 20:11:20 +0200

    Trying out stuff ...

--- .github/workflows/scripts/deploy-site.sh
+++ .github/workflows/scripts/deploy-site.sh
@@ -2,9 +2,5 @@
 set -o errexit
 
 echo "::group::Generating the site documentation and deploying it"
-mvn ${MAVEN_CLI_ARGS} site site:stage scm-publish:publish-scm \
-	-Dmaven.site.deploy.skip="true" \
-	-Dscmpublish.scm.branch="gh-pages" \
-	-Dusername="${GITHUB_ACTOR}" \
-	-Dpassword="${GITHUB_TOKEN}"
+mvn ${MAVEN_CLI_ARGS} clean site-deploy
 echo "::endgroup::"
