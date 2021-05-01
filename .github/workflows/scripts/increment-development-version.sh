377b1811930de8a038b408944efe4c2b7cea8525
Mirko Wagner <seelenvirtuose@gmail.com>
2021-05-01 14:47:46 +0200

    Refactored names of workflow building blocks.

--- .github/workflows/scripts/increment-development-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-echo "::group::Incrementing development version"
-mvn ${MAVEN_DEFAULT_ARGS} build-helper:parse-version versions:set versions:commit -DnewVersion="${NEXT_DEVELOPMENT_VERSION}"
-git commit --all --message="${GIT_COMMIT_MESSAGE}"
-git push
-echo "::endgroup::"
--- .github/workflows/scripts/maven-create-release.sh
+++ .github/workflows/scripts/maven-create-release.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 source .github/workflows/scripts/check-maven-distribution-settings.sh
 
-echo "::group::Creating the release and distributing it"
+echo "::group::Creating the Maven release package and distributing it"
 mvn ${MAVEN_DEFAULT_ARGS} build-helper:parse-version release:prepare release:perform \
 	-Darguments="${MAVEN_DEFAULT_ARGS} ${MAVEN_ADDITIONAL_ARGS_FOR_RELEASING_PACKAGE}" \
 	-Dgoals="deploy" \
