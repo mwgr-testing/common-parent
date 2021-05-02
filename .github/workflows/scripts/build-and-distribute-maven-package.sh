cd24c8bddef9fb15e786d36f65bf51154e9750b8
Mirko Wagner <seelenvirtuose@gmail.com>
2021-05-02 11:33:48 +0200

    Bugfix.

--- .github/workflows/scripts/build-and-distribute-maven-package.sh
+++ .github/workflows/scripts/build-and-distribute-maven-package.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 source .github/workflows/scripts/check-maven-distribution-settings.sh
 
-echo "::group::Building the Maven package and distributing it"
+echo "::group::Building the package and distributing it"
 mvn ${MAVEN_DEFAULT_ARGS} ${MAVEN_ADDITIONAL_ARGS} clean deploy
 echo "::endgroup::"
